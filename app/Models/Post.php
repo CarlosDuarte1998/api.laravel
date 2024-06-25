07bdc5b88ce1a0e18f0561131943bb92f5976cb8
Carlos Alexander Duarte Hernadez <64160994+CarlosDuarte1998@users.noreply.github.com>
2024-06-25 15:10:31 -0600

    Clase 8: Generando relaciones

---
+++ app/Models/Post.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    const BORRADOR = 1;
+    const PUBLICADO = 2;
+
+    //Relación uno a muchos inversa
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //Relación uno a muchos inversa
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    //Relación muchos a muchos
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    //Relación uno a muchos
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
+}
